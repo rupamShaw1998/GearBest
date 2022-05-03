6beb89a656581b891b8492de05ebf4964dd56022
rupamShaw1998 <rupamshw77777@gmail.com>
2022-05-03 23:33:44 +0530

    nav updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import {Footer} from "./components/Footer"
+import {Footer} from "./components/Footer";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Navbar />
      <Footer></Footer>
     </div>
   );
