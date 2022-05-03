303acd5c23b7476d8d34b6f46adc8eba5073e7aa
rupamShaw1998 <rupamshw77777@gmail.com>
2022-05-04 00:05:45 +0530

    css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
     return (
-        <div>
+        <div id="rs">
             <div><img src="https://uidesign.gbtcdn.com/GB/images/promotion/2019/a_evan/Gearbest/logo_gearbest.png?imbypass=true" /></div>
             <div></div>
         </div>
