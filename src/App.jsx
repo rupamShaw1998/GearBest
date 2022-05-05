c41b967d2e3cbd2648b33507f1dd39057314c48d
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-05-05 15:14:12 +0530

    Static Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import {Footer} from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { LandingPage } from './components/landingPage';
+import {Cart} from "./components/pages/Cart"
 
 
 
@@ -12,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <Cart/>
       <LandingPage/>
       
         <LandingPage/>
