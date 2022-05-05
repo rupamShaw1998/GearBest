e26f88584f0875cd750dd819e272388bd5c1ea17
rupamShaw1998 <rupamshw77777@gmail.com>
2022-05-05 21:10:00 +0530

    main updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import './App.css'
 import {Footer} from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { LandingPage } from './components/landingPage';
-
+import { Routes, Route } from "react-router-dom";
+import { ProductDetail } from "./components/ProductDetail";
+import { CollectionDetail } from "./components/CollectionDetail";
 
 function App() {
   
   return (
     <div className="App">
       <Navbar />
-      <LandingPage/>
+      <Routes>
+        <Route path="" element={<LandingPage />}></Route>
+        <Route path="/product/:id" element={<ProductDetail />}></Route>
+        <Route path="/collection/:id" element={<CollectionDetail />}></Route>
+      </Routes>
      <Footer></Footer>
     </div>
   );
