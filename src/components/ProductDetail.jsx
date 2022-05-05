70e3c349ac7e4fe634209ad012032a4d68249fd2
ShaikHajira123 <97529565+ShaikHajira123@users.noreply.github.com>
2022-05-05 21:11:50 +0530

    updated details pages
    updated details pages

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -4,29 +4,30 @@ import { useParams } from "react-router-dom"
 import {useState ,useEffect} from 'react'
 
 
+
 export const ProductDetail = () => {
-    const  [product,setProduct] = useState([])
+  const [product,setProduct] = useState([])
     
-    const {id} = useParams()
-    useEffect(()=> {
-       axios.get(`http://localhost:7005/all/${id}`).then(({data})=>{
-           setProduct (data)
-       })
-    },[])
-   
+    let {id} = useParams()
+ 
+    useEffect(() => {
+      axios.get(`http://localhost:7005/all${id}`).then(({data})=>{
+          setProduct(data)
+  })
+  },[])
     return (
       <div className="product_details">
           <div className="flex_container">
               <div className="flex_image">
-                 <img className="product_image" src={product.image}/>
+                 <img className="product_image" src={product.imgUrl}/>
               </div>
               <div className="about_product">
-                 <h4 className="product_name">{product.name}</h4>
-                 <p className="product_version">{product.version}</p>
-                 <p className="product_price">`$Price : {product.price}`</p>
-                 <p className="product_shipping">`$ Shipping : `</p>
-                 <p className="product_color">`$ Color : `</p>
-                 <p className="product_size">`$Size : `</p>
+                 <h4 className="product_name">{product.title}</h4>
+              
+                 <p className="product_price">`Price : ${product.price}`</p>
+                 <p className="product_shipping">` Shipping : `</p>
+                 <p className="product_color">` Color : `</p>
+                 <p className="product_size">`Size : `</p>
               </div>
              
           </div>
