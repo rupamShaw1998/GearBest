58b5282977b01d65cd481a4688ddb863c832e115
ShaikHajira123 <97529565+ShaikHajira123@users.noreply.github.com>
2022-05-09 18:20:04 +0530

    url changed

--- src/components/LaptopDetail.jsx
+++ src/components/LaptopDetail.jsx
@@ -16,13 +16,13 @@ export const LaptopDetail = () => {
     let {id} = useParams()
  
     useEffect(() => {
-      axios.get(`http://localhost:7005/laptop/${id}`).then(({data})=>{
+      axios.get(`https://morning-scrubland-78864.herokuapp.com/laptop/${id}`).then(({data})=>{
           setProduct(data)
   })
   },[])
 console.log(product)
   const setData=()=>{
-    fetch(`http://localhost:7005/addtocart`,{
+    fetch(`https://morning-scrubland-78864.herokuapp.com/addtocart`,{
     method:'POST',
     headers:{
       "Content-Type":"application/json"
@@ -33,7 +33,7 @@ console.log(product)
   }
 
   const setOrder=()=>{
-    fetch(`http://localhost:7005/placeorder`,{
+    fetch(`https://morning-scrubland-78864.herokuapp.com/placeorder`,{
     method:'POST',
     headers:{
       "Content-Type":"application/json"
